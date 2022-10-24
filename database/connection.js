e74904f229f083cd807afe4af302b05323baa3bb
Daniel Díaz <danielnochess@gmail.com>
2022-10-24 16:15:09 -0500

    changes to work in local database

--- database/connection.js
+++ database/connection.js
@@ -1,20 +1,27 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-{
-    host: process.env.DB_HOST,
+// PlaneScale
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+// {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     dialectOptions: {
+//         ssl: {
+//             rejectUnauthorized: true,
+//         },
+//     },
+// },
+// );
+
+const sequelize = new Sequelize('reminder', 'root', 'mysqlubuntu', {
+    host: 'localhost',
     dialect: 'mysql',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: true,
-        },
-    },
-},
-);
+    logging: false,
+  });
 
 module.exports = {
     sequelize,
